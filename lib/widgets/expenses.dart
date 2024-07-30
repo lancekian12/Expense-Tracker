d16416bbf29ee8b96ea60b3fa1e8d00a4d8611e6
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-07-30 22:46:20 +0800

    Show text when there is no expense anymore and show info message and undo button when the expense is swiped away

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -34,12 +34,6 @@ class _ExpendesState extends State<Expenses> {
     });
   }
 
-  void _removeExpense(Expense expense) {
-    setState(() {
-      _registerdExpenses.remove(expense);
-    });
-  }
-
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       isScrollControlled: true,
@@ -50,8 +44,39 @@ class _ExpendesState extends State<Expenses> {
     );
   }
 
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registerdExpenses.indexOf(expense);
+    setState(() {
+      _registerdExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense Deleted'),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registerdExpenses.insert(expenseIndex, expense);
+              });
+            }),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some'),
+    );
+
+    if (_registerdExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registerdExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense tracker'),
@@ -65,10 +90,7 @@ class _ExpendesState extends State<Expenses> {
       body: Column(
         children: [
           Expanded(
-            child: ExpensesList(
-              expenses: _registerdExpenses,
-              onRemoveExpense: _removeExpense,
-            ),
+            child: mainContent,
           ),
         ],
       ),
